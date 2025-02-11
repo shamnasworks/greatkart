9b148c704b4feecf6f8dc117bbd118ae396b51ff
Shamna minas <shamnaswork@gmail.com>
2025-02-11 16:22:29 +0530

    Accounts app and category addition
    Accounts app is created and  done category addition from admin

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'category',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -70,6 +71,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'greatkart.wsgi.application'
 
+AUTH_USER_MODEL = 'accounts.Account'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
