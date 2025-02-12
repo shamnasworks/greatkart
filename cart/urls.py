6a53c75fa5b32bd6a2c81e53c15da3e805236e51
Shamna minas <shamnaswork@gmail.com>
2025-02-12 15:59:30 +0530

    added cart
    cart

---
+++ cart/urls.py
@@ -0,0 +1,15 @@
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path,include
+from  .import views
+
+urlpatterns = [
+    path('', views.cart,name='cart'),
+   
+    
+    
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- greatkart/urls.py
+++ greatkart/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('',views.home,name='home'),
     # path('accounts/', include('accounts.urls')),
     path('store/', include('store.urls')),
-    
+    path('cart/', include('cart.urls')),
     
 ]
 
